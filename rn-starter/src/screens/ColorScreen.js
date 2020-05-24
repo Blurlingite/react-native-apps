3a5121eba7c509774d7ecc1fe7493658d14a2fca
Vishnu Dawah <vkdawah@gmail.com>
2020-05-24 14:22:33 -0400

    Add ability to add to color's rgb values

--- rn-starter/src/screens/ColorScreen.js
+++ rn-starter/src/screens/ColorScreen.js
@@ -4,7 +4,6 @@ import { StyleSheet, View, Button, FlatList } from "react-native";
 const ColorScreen = () => {
   const [colors, setColors] = useState([]);
 
-  console.log(colors);
   return (
     <View>
       <Button
