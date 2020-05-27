e48d867c979985116cafe121b95a17074d8dad35
Vishnu Dawah <vkdawah@gmail.com>
2020-05-27 11:09:32 -0400

    Add navigation to a results screen when clicking on any image

---
+++ food/src/screens/ResultsShowScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+const ResultsShowScreen = () => {
+  return (
+    <View>
+      <Text>RESULTS SHOW</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultsShowScreen;
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
-import SearchBar from "./components/SearchBar";
+import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
-import ResultsList from "./components/ResultsList";
+import ResultsList from "../components/ResultsList";
 
-const SearchScreen = () => {
+const SearchScreen = ({ navigation }) => {
   const [term, setTerm] = useState("");
   const [searchApi, results, errorMessage] = useResults();
 
@@ -26,11 +26,17 @@ const SearchScreen = () => {
         <ResultsList
           results={filterResultsByPrice("$")}
           title="Cost Effective"
+          navigation={navigation}
         />
-        <ResultsList results={filterResultsByPrice("$$")} title="Bit Pricier" />
+        <ResultsList
+          results={filterResultsByPrice("$$")}
+          title="Bit Pricier"
+          navigation={navigation}
+        />
         <ResultsList
           results={filterResultsByPrice("$$$")}
           title="Big Spender!"
+          navigation={navigation}
         />
       </ScrollView>
     </>
